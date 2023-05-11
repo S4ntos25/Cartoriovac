41d5eb1266ad5f64f08e774fc344ae2f1c6e7571
S4ntos25 <132910423+S4ntos25@users.noreply.github.com>
2023-05-11 17:23:01 -0300

    upload do projeto
    Projeto completo de acordo com as especificações do cliente

---
+++ Finalidado.cpp
@@ -0,0 +1,166 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <locale.h> 
+#include <string.h>
+		
+int registro()
+{
+	char arquivo[40];
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+	char cargo[40];
+	
+	printf("Digite o CPF a ser cadastrado: ");
+	scanf("%s", cpf);
+	
+	strcpy(arquivo, cpf);
+	
+	FILE *file; 
+	file = fopen(arquivo, "w");
+	fprintf(file,cpf); 
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o nome a ser cadastrado: ");
+	scanf("%s",nome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,nome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o sobrenome a ser cadastrado: ");
+	scanf("%s",sobrenome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,sobrenome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o cargo a ser cadastrado: ");
+	scanf("%s",cargo);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,cargo);
+	fclose(file);	
+	
+    system("pause");
+
+}
+
+int consulta()
+{
+	setlocale(LC_ALL, "Portuguese"); 
+	char cpf[40];
+	char conteudo[100];
+	
+	printf("Digite o CPF a ser consultado: ");
+	scanf("%s",cpf);
+  	
+	FILE *file;
+	file = fopen(cpf,"r");
+	if(file == NULL)
+{
+	printf("Não foi possivel abrir o arquivo, não localizado!.\n");
+}
+while(fgets(conteudo, 100, file) != NULL)
+{
+	printf("\nEssas são as informações do usuario: ");
+	printf("%s", conteudo);
+	printf("\n\n");
+}
+  	
+system ("pause");
+}
+
+int deletar()
+{
+	char cpf[40];
+	
+	printf("Digite o cpf a ser deletado:\n");
+	scanf("%s",cpf);
+	
+	remove(cpf);
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	
+	if(file == NULL)
+	{
+		printf("O usuário não foi encontrado!.\n");
+		system("pause");
+	}	
+}
+
+
+	
+int main()
+{
+	int opcao=0;
+	int laco=1;
+	char senhadigitada[10]="a";
+	int comparacao;
+	
+	printf("Login de adminstrador:");
+	scanf("%s",senhadigitada);
+	
+	comparacao = strcmp(senhadigitada, "admin");
+	
+	if(comparacao == 0)
+	{
+		for(laco=1;laco=1;)
+		{
+	
+			system("cls");
+	
+			setlocale(LC_ALL, "Portuguese");
+				 
+			printf("Escolha a opção desejada do menu\n\n");
+			printf("\t1 - Registrar nomes\n");
+			printf("\t2 - Consultar nomes\n");
+			printf("\t3 - Deletar nomes\n\n");
+			printf("t4 - Sair do progrma \n\n"); 
+			printf("Opção: ");
+					
+			scanf("%d", &opcao);
+			
+			system("cls");
+		
+			switch(opcao)
+			{
+				case 1:
+				registro();
+				break;
+				
+				case 2:
+				consulta();
+				break;
+			
+				case 3:
+				deletar();
+				break;
+				
+				case 4:
+				printf("Obrigado por usar por usar");
+				return 0;
+				break;
+			
+				default:
+				printf("Essa opição não esta disponivel!\n");
+				system("pause");
+				break;
+			} 
+				
+		}	
+	}
+}
